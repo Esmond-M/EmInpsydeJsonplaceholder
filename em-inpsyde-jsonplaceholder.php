ba3311ca034ae5a117f52b8bc5443aefb604e80b
Esmond <esmondmccain@gmail.com>
2020-03-16 12:50:19 -0500

    added initial html and ajax request functions

--- em-inpsyde-jsonplaceholder.php
+++ em-inpsyde-jsonplaceholder.php
@@ -7,7 +7,7 @@
  * Author: Esmond Mccain
  * Author URI: https://esmondmccain.com
 
-    @category Wordpress-plugin
+    @category Wordpress_Plugin
     @package  Esmond-M
     @author   Esmond Mccain <esmondmccain@gmail.com>
     @license  https://www.gnu.org/licenses/gpl-3.0.en.html GNU General Public License
--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ if (!class_exists('EM_Inpsyde_Jsonplaceholder')) {
     /**
     Declaring class
 
-    @category Wordpress
+    @category Wordpress_Plugin
     @package  Esmond-M
     @author   Esmond Mccain <esmondmccain@gmail.com>
     @license  https://www.gnu.org/licenses/gpl-3.0.en.html GNU General Public License
@@ -14,19 +14,19 @@ if (!class_exists('EM_Inpsyde_Jsonplaceholder')) {
 
     class EM_Inpsyde_Jsonplaceholder
     {
-
         private static $_instance;
         /**
         Declaring constructor
          */
         private function __construct()
         {
-
+            add_filter('the_content', array($this, 'emCustomEndpointUrl'));
+            add_action('wp_enqueue_scripts', array($this, 'emInpsydeJsonplaceholderScripts'));
         }
         /**
         Declaring function for class instance
 
-        @return param
+        @return self
          */
         public static function instance()
         {
@@ -37,6 +37,43 @@ if (!class_exists('EM_Inpsyde_Jsonplaceholder')) {
             return self::$_instance;
         }
 
+        /**
+        Custom endpoint url function
+
+        @return void
+         */
+        public function emCustomEndpointUrl()
+        {
+            $get_inpsyde_api = $_GET['inpsyde-api'];
+            if (isset($get_inpsyde_api)) {
+                ?>
+
+                <input id="btn-load-json" type="button" value="Render JSON" id="Render JSON" />
+                <br/>
+                <table id="jsonTable" style="visibility:hidden">
+                    <thead>
+                    <tr>
+                        <!-- The JSON downloaded from the URL above provides four attributes.-->
+                        <th>id</th>
+                        <th>name</th>
+                        <th>username</th>
+                    </tr>
+                    </thead>
+                </table>
+                <?php
+            }
+        }
+
+        /**
+        Load scripts and/or styles for plugin
+
+        @return void
+         */
+        public function emInpsydeJsonplaceholderScripts()
+        {
+            $rand = rand(1, 99999999999);
+            wp_enqueue_script('em-Inpsyde-Jsonplaceholder-Scripts', plugin_dir_url(__FILE__) . 'lib/general.js', array('jquery'), $rand, true);
+        }
     }
     EM_Inpsyde_Jsonplaceholder::instance();
 }
